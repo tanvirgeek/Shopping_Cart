ba29bf88379fda711f2a4c9c7400a69739c1c5b0
tanvirgeek <tanvirgeek@gmail.comm>
2020-04-08 08:10:16 +0600

    Day2 nodejs

--- myApp/routes/admin.js
+++ myApp/routes/admin.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET admin page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HOME Working' });
+  res.render('admin', { title: 'Admin Page Workin' });
 });
 
 module.exports = router;
